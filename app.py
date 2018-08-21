aea79d1505910e8783ec6c5b28bbb19b59df8196
danidavid <33230485+danidavid@users.noreply.github.com>
2018-08-21 23:26:58 +0500

    working and testing

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask_restful import Api, Resource, abort, reqparse
 app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
 
-Todo = {}
+Todo = {
+    'todo1':'python work',
+    'todo2':'assignments'
+}
 
 
 class Todo_task(Resource):
     
 
-    def get(self,todo_id,task):
-        return Todo[todo_id] == str(task)
+    def get(self):
+        return Todo
     
     def post(self,todo_id):
         if todo_id not in Todo:
@@ -24,7 +27,7 @@ class Todo_task(Resource):
 
         return 'Success!'    
 
-api.add_resource(Todo_task, '/<string:todo_id>')
+api.add_resource(Todo_task, '/')
 
 if __name__ == "__main__":
     app.run(host='localhost',port='51210',debug=True) 
