e52d5f7145ddee018ca5df3b7334d86e00bd602e
danidavid <33230485+danidavid@users.noreply.github.com>
2018-08-26 20:24:39 +0500

    updated these files (read carefully at the bottom of client.py)

---
+++ app_server2.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, abort, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+Todo = {
+    'todo1':'python work',
+    'todo2':'assignments'
+}
+
+
+class Todo_review(Resource):
+	""" to see the contents of task in index page """
+
+	def get(self):
+		""" showing all tasks """
+		return Todo
+
+class Todo_task(Resource):
+    """ to add tasks and show them individually """
+
+    def get(self,todo):
+        """ showing specific tasks """
+
+        if todo not in Todo:
+            abort(404, message="no task is here!")
+
+        return Todo[todo]
+
+
+    def put(self,todo):
+        """ updating tasks """
+
+
+        if todo not in Todo:
+            Todo[todo] = request.form['data']
+        else:
+            # making this situation so that it will not overwrite
+            abort(404,message="task will not override")
+
+
+    def post(self,todo):
+        """ adding a new task """
+        Todo[todo] = request.form['data']
+
+
+        print ('Success!')
+        return {todo : Todo[todo]}
+
+
+api.add_resource(Todo_task, '/<string:todo>')
+api.add_resource(Todo_review, '/')
+
+if __name__ == "__main__":
+    app.run(host='localhost',port='8000',debug=True)
--- client.py
+++ client.py
@@ -1,3 +1,34 @@
 from requests import get,put
 
-get("http://localhost/51210")
+## in order to add new task
+
+put('http://localhost:8000/todo4',data={'data': 'Remember the milk'}).json()
+
+# in order to get requests
+
+msg = get("http://localhost:8000").json()
+print(msg)
+
+# as two tasks are in the server
+todo1 = get("http://localhost:8000/todo1").json()
+print(todo1)
+todo2 = get("http://localhost:8000/todo2").json()
+print(todo2)
+
+# the third task is not available, therefore
+# it will show message regarding "no task is here!"
+
+todo3 = get("http://localhost:8000/todo3").json()
+print(todo3)
+
+# in order to overwrite these tasks you
+# can use the same command in line 5, but
+# changes are there regarding app_server files
+# be sure to check them out.
+
+# app_server1.py will overwrite easily
+# app_server2.py will not overwrite easily
+
+# future enhancements will be given to
+# administrator for overriding tasks if
+# todo lists is made in collaboration with team
